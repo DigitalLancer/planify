94577d3b4457445472f92bde352f811c44b206e0
DigitalLancer <ahmetsalimusul@yahoo.com>
2026-03-03 10:42:50 +0300

    add mock json

--- frontend/components/dashboard/DashboardEventList.tsx
+++ frontend/components/dashboard/DashboardEventList.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import React from 'react'
 import type { EventItem } from '@/types/event'
 import DashboardEventListItem from './DashboardEventListItem'
-
+import { useState, useEffect } from 'react'
 
 function DashboardEventList() {
+    const [events, setEvents] = useState<EventItem[]>([])
+
+    useEffect(() => {
+        fetch('/mock/event.json')
+            .then((r) => r.json())
+            .then(setEvents)
+    }, [])
+
     const eventList: EventItem[] = [
         { id: 1, title: "Piknik", startDate: "10.03.2026", status: "upcoming" },
         { id: 2, title: "Arkadaşlarla Kahve", startDate: "12.03.2026", status: "upcoming" },
@@ -11,9 +21,9 @@ function DashboardEventList() {
     ]
 
     return (
-        <div className='mt-3 flex flex-col gap-4'>
-            {eventList.map(event => (
-                <DashboardEventListItem data={event} key={event.id}/>
+        <div className='mt-3 flex flex-col gap-4 overflow-hidden'>
+            {events.map(event => (
+                <DashboardEventListItem data={event} key={event.id} />
             ))}
         </div>
     )
