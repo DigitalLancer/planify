86d8f8fd883b3fcc1b19bee62c5ce5792c685fa3
Ahmet USUL <ahmet.usul@oyakdijital.com.tr>
2026-03-30 15:02:16 +0300

    Cascade policy

---
+++ backend/Planify.API/Planify.API/Migrations/20260330115912_AddUserEventCascadePolicy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Planify.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserEventCascadePolicy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
